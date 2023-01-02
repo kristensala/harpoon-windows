f66001f7acc3c0455c984b823878ebc68f742f5d
Kristen Sala <salakristen1@gmail.com>
2023-01-02 03:25:56 +0200

    test1 fixing windows pathing

--- lua/harpoon/ui.lua
+++ lua/harpoon/ui.lua
@@ -174,8 +174,8 @@ function M.nav_file(id)
 
     local mark = Marked.get_marked_file(idx)
     local filename = mark.filename
-    if filename:sub(1, 1) ~= "/" then
-        filename = vim.loop.cwd() .. "/" .. mark.filename
+    if filename:sub(1, 1) ~= "\\" then
+        filename = vim.loop.cwd() .. "\\" .. mark.filename
     end
     local buf_id = get_or_create_buffer(filename)
     local set_row = not vim.api.nvim_buf_is_loaded(buf_id)
