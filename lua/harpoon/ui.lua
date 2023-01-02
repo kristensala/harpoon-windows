69e9d0d9425e724815d19b681246545d2ac19f6e
Kristen Sala <salakristen1@gmail.com>
2023-01-02 03:37:35 +0200

    test3

--- lua/harpoon/ui.lua
+++ lua/harpoon/ui.lua
@@ -173,7 +173,12 @@ function M.nav_file(id)
     end
 
     local mark = Marked.get_marked_file(idx)
-    local filename = vim.fs.normalize(mark.filename)
+    local filename = mark.filename
+    if filename:sub(1, 1) ~= "/" then
+        filename = vim.loop.cwd() .. "/" .. mark.filename
+    end
+
+    vim.fs.normalize(filename)
 
     local buf_id = get_or_create_buffer(filename)
     local set_row = not vim.api.nvim_buf_is_loaded(buf_id)
